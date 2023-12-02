84fe2bd9aa38ff2190f02f4123b2c0e1d5532d83
Myron_dev <85976759+myron98980@users.noreply.github.com>
2023-12-02 02:59:46 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ VIDEOS_DICT = {
 
 # ML Model config
 MODEL_DIR = ROOT / 'weights'
-DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
+DETECTION_MODEL = MODEL_DIR / 'yolov5s.pt'
 SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
 
 # Webcam
