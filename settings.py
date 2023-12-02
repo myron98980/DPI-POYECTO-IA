5590340fb71ab4523a8ec337cf1023c713a5a56b
Myron_dev <85976759+myron98980@users.noreply.github.com>
2023-12-02 09:12:28 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 VIDEO_DIR = ROOT / 'videos'
 VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
 VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
-VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
+VIDEO_3_PATH = VIDEO_DIR / 'vlc-record-2023-12-02-09h08m20s-cuchillo.mp4-.mp4'
 VIDEOS_DICT = {
     'video_1': VIDEO_1_PATH,
     'video_2': VIDEO_2_PATH,
